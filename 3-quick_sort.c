6344498119051f5f425176700615108114827f21
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2023-12-22 06:52:05 +0000

    quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -24,30 +24,35 @@ void quick_sort(int *array, size_t size)
  */
 int partition(int *array, int lo, int hi, size_t size)
 {
-	int i = lo - 1, j = lo;
-	int pivot = array[hi], aux = 0;
+    if (lo < hi)
+    {
+        int i = lo - 1, j = lo;
+        int pivot = array[hi], aux = 0;
 
-	for (; j < hi; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (array[i] != array[j])
-			{
-				aux = array[i];
-				array[i] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
-		}
-	}
-	if (array[i + 1] != array[hi])
-	{
-		aux = array[i + 1];
-		array[i + 1] = array[hi];
-		print_array(array, size);
-	}
-	return (i + 1);
+        for (; j < hi; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;
+                if (array[i] != array[j])
+                {
+                    aux = array[i];
+                    array[i] = array[j];
+                    array[j] = aux;
+                    print_array(array, size);
+                }
+            }
+        }
+        if (array[i + 1] != array[hi])
+        {
+            aux = array[i + 1];
+            array[i + 1] = array[hi];
+            array[hi] = aux;
+            print_array(array, size);
+        }
+        return (i + 1);
+    }
+    return -1;
 }
 
 /**
@@ -61,13 +66,13 @@ int partition(int *array, int lo, int hi, size_t size)
 
 void quick_s(int *array, int lo, int hi, size_t size)
 {
-	int pivot;
+    int pivot;
 
-	if (lo < hi)
-	{
-		pivot = partition(array, lo, hi, size);
-		quick_s(array, lo, pivot - 1, size);
-		quick_s(array, pivot + 1, hi, size);
-	}
+    if (lo < hi)
+    {
+        pivot = partition(array, lo, hi, size);
+        quick_s(array, lo, pivot - 1, size);
+        quick_s(array, pivot, hi, size);  // Exclude pivot from the next call
+    }
 }
 
